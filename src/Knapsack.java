c5091c809df8e3bd22ee086091494990bb9f60b1
Dominik Vesely <vesely@dominoo.cz>
2013-09-29 18:27:47 +0200

    clean up

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -6,15 +6,14 @@ import java.util.concurrent.TimeUnit;
 
 import com.sun.istack.internal.FinalArrayList;
 
-
 class KnapsackProblem {
-	
+
 	KnapsackSolution solution;
 	int weights[];
 	int costs[];
 	int maxWeight;
-	int id;	
-	
+	int id;
+
 	public KnapsackProblem(int[] weights, int[] costs, int maxWeight, int id) {
 		super();
 		this.weights = weights;
@@ -23,185 +22,155 @@ class KnapsackProblem {
 		this.id = id;
 		this.solution = new KnapsackSolution(this.weights.length);
 	}
-	
+
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder(this.id+" "+this.weights.length + " " + solution.cost);
-		for(int b : solution.precense ) {
-			sb.append(" "+b);
+		StringBuilder sb = new StringBuilder(this.id + " "
+				+ this.weights.length + " " + solution.cost);
+		for (int b : solution.precense) {
+			sb.append(" " + b);
 		}
 		return sb.toString();
 	}
-	
+
 }
 
-class KnapsackSolution{
+class KnapsackSolution {
 	int precense[];
-	int cost;		
+	int cost;
 	long time;
-	
+
 	public KnapsackSolution(int size) {
 		super();
 		this.cost = 0;
 		this.precense = new int[size];
-	}	
+	}
 }
 
 public class Knapsack {
 
-	
 	String testFile = "inst/knap_25.inst.dat";
-	Scanner sc= null;
+	Scanner sc = null;
 	KnapsackProblem currentProblem;
 	int mask[];
 	int currentCost;
-	
-	
+
 	public void run() throws FileNotFoundException {
 		sc = new Scanner(new File(testFile));
-	//	sc = new Scanner(System.in);
-		
-		while(sc.hasNextLine()) {
-			int id,n ,m;			
+		// sc = new Scanner(System.in);
+
+		while (sc.hasNextLine()) {
+			int id, n, m;
 			String line = sc.nextLine();
 			String[] split = line.split(" ");
-			id =Integer.parseInt(split[0]);
+			id = Integer.parseInt(split[0]);
 			n = Integer.parseInt(split[1]);
 			m = Integer.parseInt(split[2]);
 			int w[] = new int[n];
-			int c[] = new int[n];			
-			
-			for(int i = 0; i < n; i++) {
-				int index = 3 + (i*2); //offsetv line
+			int c[] = new int[n];
+
+			for (int i = 0; i < n; i++) {
+				int index = 3 + (i * 2); // offsetv line
 				w[i] = Integer.parseInt(split[index]);
-				c[i] = Integer.parseInt(split[index+1]);
+				c[i] = Integer.parseInt(split[index + 1]);
 			}
-			
-			//RunBruteforce problem
+
+			// RunBruteforce problem
 			// obtain result and print it
-			bruteForceProblem(id,n,m,w,c);
+			bruteForceProblem(id, n, m, w, c);
 
 		}
-		
+
 	}
-	
-	
+
 	private KnapsackProblem bruteForceProblem(int id, int n, int m, int[] w2,
 			int[] c) {
-		
-		
+
 		KnapsackProblem problem = new KnapsackProblem(w2, c, m, id);
 		currentProblem = problem;
 		this.mask = new int[c.length];
 
 		long start = System.nanoTime();
-		
-		knapsackBF(c.length-1,0,0,0);
-		knapsackBF(c.length-1,1,0,0);
-		
+
+		knapsackBF(c.length - 1, 0, 0, 0);
+		knapsackBF(c.length - 1, 1, 0, 0);
+
 		long end = System.nanoTime();
-		
-		
+
 		System.out.println(problem);
-		System.out.println(TimeUnit.SECONDS.convert((end-start),TimeUnit.NANOSECONDS));
-		/*System.out.print(id + " "+ n +" "+ res);
-		for(int i = 0; i < n;i++) {
-			System.out.print(" " +problem.solution.pr[i]);
-		}
-		System.out.println("");*/
+		System.out.println(TimeUnit.SECONDS.convert((end - start),
+				TimeUnit.NANOSECONDS));
+		/*
+		 * System.out.print(id + " "+ n +" "+ res); for(int i = 0; i < n;i++) {
+		 * System.out.print(" " +problem.solution.pr[i]); }
+		 * System.out.println("");
+		 */
 		return problem;
-		
+
 	}
-	
-	
 
-private void knapsackBF(int index, int state) {
-	
-	if(index >= 0) {		
-		this.mask[index] = state;		
-		knapsackBF(index-1, 0);
-		knapsackBF(index-1, 1);	
-		
-	} else {		
-		int cost =  sumhelper(mask, this.currentProblem.costs);
-		if(cost > this.currentProblem.solution.cost && sumhelper(mask, this.currentProblem.weights) <= this.currentProblem.maxWeight ) {
-			this.currentProblem.solution.cost = sumhelper(mask, this.currentProblem.costs);
-			this.currentProblem.solution.precense = this.mask.clone();
-		}
-		
-	}
-	
-}
+	/*
+	 * This method is slow and deprecated :)
+	 * 
+	 * private void knapsackBF(int index, int state) {
+	 * 
+	 * if(index >= 0) { this.mask[index] = state; knapsackBF(index-1, 0);
+	 * knapsackBF(index-1, 1);
+	 * 
+	 * } else { int cost = sumhelper(mask, this.currentProblem.costs); if(cost >
+	 * this.currentProblem.solution.cost && sumhelper(mask,
+	 * this.currentProblem.weights) <= this.currentProblem.maxWeight ) {
+	 * this.currentProblem.solution.cost = sumhelper(mask,
+	 * this.currentProblem.costs); this.currentProblem.solution.precense =
+	 * this.mask.clone(); }
+	 * 
+	 * }
+	 * 
+	 * }
+	 */
 
-private void knapsackBF(int index, int state, int w, int c) {
-	
-	if(index >= 0) {		
-		this.mask[index] = state;
-		if(state == 1) {
-			w+=this.currentProblem.weights[index];
-			c+=this.currentProblem.costs[index];
-		}
-		knapsackBF(index-1, 0,w,c);
-		knapsackBF(index-1, 1,w,c);	
-		
-	} else {		
-		//First I try to compare cost because it is more likely to fail this assert
-		if(c > this.currentProblem.solution.cost && w <= this.currentProblem.maxWeight ) {
-			this.currentProblem.solution.cost = c;
-			this.currentProblem.solution.precense = this.mask.clone();
-		}		
-	}
-	
-}
+	private void knapsackBF(int index, int state, int w, int c) {
 
+		if (index >= 0) {
+			this.mask[index] = state;
+			if (state == 1) {
+				w += this.currentProblem.weights[index];
+				c += this.currentProblem.costs[index];
+			}
+			knapsackBF(index - 1, 0, w, c);
+			knapsackBF(index - 1, 1, w, c);
 
-int sumhelper(int mask[], int list[]) {
-	int sum= 0;
-	for(int i = 0; i < mask.length ; i++) {
-		if(mask[i] == 1) sum += list[i];
-	}
-	return sum;
-}
-
-
-private int knapsack(int i, int W) {
-    if (i < 0) {
-        return 0;
-    }
-    
-    if (this.currentProblem.weights[i] > W) {
-		this.currentProblem.solution.precense[i] = 0;
-        return knapsack(i-1, W);
-    } else {
-		int left = knapsack(i-1, W);
-		int right = knapsack(i-1, W - (this.currentProblem.weights[i])) + (this.currentProblem.costs[i]);
-		if(right >= left) {
-			this.currentProblem.solution.precense[i] = 1;
-			return right;
 		} else {
-			
-			this.currentProblem.solution.precense[i] = 0;
-			return left;
+			// First I try to compare cost because it is more likely to fail
+			// this assert
+			if (c > this.currentProblem.solution.cost
+					&& w <= this.currentProblem.maxWeight) {
+				this.currentProblem.solution.cost = c;
+				this.currentProblem.solution.precense = this.mask.clone();
+			}
 		}
-	
-    }
-}
 
-	
-	
+	}
 
-
+	int sumhelper(int mask[], int list[]) {
+		int sum = 0;
+		for (int i = 0; i < mask.length; i++) {
+			if (mask[i] == 1)
+				sum += list[i];
+		}
+		return sum;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String args[]) throws Exception {
-		(new Knapsack()).run();	
+		(new Knapsack()).run();
 
 	}
 
 }
 
 class Solution {
-	
+
 }
